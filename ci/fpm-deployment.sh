1ea9e800dcf986befaf648852e36d700d6adf1c8
14NGiestas <iangiestaspauli@gmail.com>
2022-01-06 01:26:29 -0300

    Attempt to implement something like the stdlib-fpm workflow

--- ci/fpm-deployment.sh
+++ ci/fpm-deployment.sh
@@ -24,16 +24,14 @@ include=(
 prune=(
 )
 
-mkdir -p "$destdir/src" "$destdir/test"
-
 # Preprocess mfi sources
 find src -maxdepth 1 -iname "*.fypp" \
   | cut -f1 -d. | xargs -P "$njob" -I{} "$fypp" "{}.fypp" "$destdir/{}.f90" $fyflags
 
 # Collect mfi source files
 find src -maxdepth 1 -iname "*.f90" -exec cp {} "$destdir/src/" \;
-find src/tests -name "test_*.f90" -exec cp {} "$destdir/test/" \;
-find src/tests -name "*.dat" -exec cp {} "$destdir/" \;
+find test -name "test_*.f90" -exec cp {} "$destdir/test/" \;
+find test -name "*.dat" -exec cp {} "$destdir/" \;
 
 # Include additional files
 cp "${include[@]}" "$destdir/"
