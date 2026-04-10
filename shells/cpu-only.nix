4d0349437afac9685f380ce691ca0c4cd825f5db
Ian Giestas Pauli <iangiestas@usp.br>
2026-04-10 17:15:18 -0300

    ci: Implement exhaustive matrix testing with Nix

--- shells/cpu-only.nix
+++ shells/cpu-only.nix
@@ -1,38 +1,24 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz") { config.allowUnfree = true; } }:
 
 let
-  # Use CUDA 11.8 which is compatible with driver 470.x (CUDA 11.4)
-  cudaPkgs = pkgs.cudaPackages_11_8;
-  
   # Libraries needed for both build and runtime
-  cudaLibs = [
-    cudaPkgs.libcublas
-    cudaPkgs.libcublas.dev
-    cudaPkgs.libcusolver
-    cudaPkgs.libcusolver.dev
-    cudaPkgs.cuda_cudart
-    cudaPkgs.cuda_cudart.dev
-    cudaPkgs.cuda_nvcc
-  ];
-
-  # All libraries to be included in build and runtime paths
   allLibs = [
     pkgs.hdf5
     pkgs.hdf5-fortran
     pkgs.blas
     pkgs.lapack
-  ] ++ cudaLibs;
+  ];
 in
 pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.pkg-config 
-    #pkgs.fortran-fpm
+    pkgs.gfortran
+    pkgs.fortran-fpm
   ];
 
   buildInputs = allLibs;
 
   shellHook = ''
-    # Set aliases manually since programs.zsh doesn't work here
     alias fpm="fortran-fpm"
     
     # Build-time and Run-time: Help gfortran and the dynamic linker find all libraries
---
+++ shells/gpu-ready.nix
@@ -0,0 +1,47 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz") { config.allowUnfree = true; } }:
+
+let
+  # Use CUDA 11.8 which is compatible with driver 470.x (CUDA 11.4)
+  cudaPkgs = pkgs.cudaPackages_11_8;
+  
+  # Libraries needed for both build and runtime
+  cudaLibs = [
+    cudaPkgs.libcublas
+    cudaPkgs.libcublas.dev
+    cudaPkgs.libcusolver
+    cudaPkgs.libcusolver.dev
+    cudaPkgs.cuda_cudart
+    cudaPkgs.cuda_cudart.dev
+    cudaPkgs.cuda_nvcc
+  ];
+
+  # All libraries to be included in build and runtime paths
+  allLibs = [
+    pkgs.hdf5
+    pkgs.hdf5-fortran
+    pkgs.blas
+    pkgs.lapack
+  ] ++ cudaLibs;
+in
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.pkg-config 
+    pkgs.gfortran
+    pkgs.fortran-fpm
+  ];
+
+  buildInputs = allLibs;
+
+  shellHook = ''
+    alias fpm="fortran-fpm"
+    
+    # Build-time and Run-time: Help gfortran and the dynamic linker find all libraries
+    export LIBRARY_PATH="${pkgs.lib.makeLibraryPath allLibs}:$LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath allLibs}:$LD_LIBRARY_PATH"
+    
+    # Optional: Automatically enter zsh if not already in it
+    if [[ $- == *i* && $SHELL != *"zsh"* ]]; then
+      exec zsh
+    fi
+  '';
+}
