8d96b2e1c9286ed0041aeb914fda3b395f6dfaaf
14NGiestas <iangiestaspauli@gmail.com>
2026-04-13 00:12:43 +0000

    Deploying to docs from @ 14NGiestas/mfi@794031f83a409392bd737b3df04b17eef1594e89 🚀

--- src/hpmv.f90
+++ src/hpmv.f90
@@ -1,42 +1,117 @@
-module f77_blas_hpmv
+module mfi_blas_hpmv
     use iso_fortran_env
+    use f77_blas
+#if defined(MFI_CUBLAS)
     use iso_c_binding
+    use mfi_blas_cublas
+#endif
+#if defined(MFI_EXTENSIONS)
+    use mfi_blas_extensions
+#endif
     implicit none
 
-!> Generic old style interface for HPMV.
+!> Generic modern interface for HPMV.
 !> Supports c, z.
-!> See also: [[mfi_hpmv]], [[chpmv]], [[zhpmv]].
-interface f77_hpmv
-!> Original interface for CHPMV
-!> See also: [[mfi_hpmv]], [[hpmv]].
-pure subroutine chpmv(uplo, n, alpha, ap, x, incx, beta, y, incy)
-    import :: REAL32
-    integer, parameter :: wp = REAL32
-    complex(REAL32), intent(in) :: ap(*)
-    complex(REAL32), intent(in) :: x(*)
-    complex(REAL32), intent(inout) :: y(*)
-    character, intent(in) :: uplo
-    complex(REAL32), intent(in) :: alpha
-    complex(REAL32), intent(in) :: beta
-    integer, intent(in) :: n
-    integer, intent(in) :: incx
-    integer, intent(in) :: incy
-end subroutine
-!> Original interface for ZHPMV
-!> See also: [[mfi_hpmv]], [[hpmv]].
-pure subroutine zhpmv(uplo, n, alpha, ap, x, incx, beta, y, incy)
-    import :: REAL64
-    integer, parameter :: wp = REAL64
-    complex(REAL64), intent(in) :: ap(*)
-    complex(REAL64), intent(in) :: x(*)
-    complex(REAL64), intent(inout) :: y(*)
-    character, intent(in) :: uplo
-    complex(REAL64), intent(in) :: alpha
-    complex(REAL64), intent(in) :: beta
-    integer, intent(in) :: n
-    integer, intent(in) :: incx
-    integer, intent(in) :: incy
-end subroutine
+!> See also:
+!> [[f77_hpmv:chpmv]], [[f77_hpmv:zhpmv]].
+interface mfi_hpmv
+    module procedure :: mfi_chpmv
+    module procedure :: mfi_zhpmv
 end interface
+
+contains
+
+!> Modern interface for [[f77_hpmv:f77_hpmv]].
+!> See also: [[mfi_hpmv]], [[f77_hpmv]].
+pure subroutine mfi_chpmv(ap, x, y, uplo, alpha, beta, incx, incy)
+    integer, parameter :: wp = REAL32
+    complex(REAL32), intent(in) :: x(:)
+    complex(REAL32), intent(in) :: ap(:)
+    complex(REAL32), intent(inout) :: y(:)
+    character, intent(in), optional :: uplo
+    character :: local_uplo
+    complex(REAL32), intent(in), optional :: alpha
+    complex(REAL32) :: local_alpha
+    complex(REAL32), intent(in), optional :: beta
+    complex(REAL32) :: local_beta
+    integer, intent(in), optional :: incx
+    integer :: local_incx
+    integer, intent(in), optional :: incy
+    integer :: local_incy
+    integer :: n
+    if (present(uplo)) then
+        local_uplo = uplo
+    else
+        local_uplo = 'U'
+    end if
+    if (present(alpha)) then
+        local_alpha = alpha
+    else
+        local_alpha = 1.0_wp
+    end if
+    if (present(beta)) then
+        local_beta = beta
+    else
+        local_beta = 0.0_wp
+    end if
+    if (present(incx)) then
+        local_incx = incx
+    else
+        local_incx = 1
+    end if
+    if (present(incy)) then
+        local_incy = incy
+    else
+        local_incy = 1
+    end if
+    n = size(x)
+    call f77_hpmv(local_uplo,n,local_alpha,ap,x,local_incx,local_beta,y,local_incy)
+end subroutine
+!> Modern interface for [[f77_hpmv:f77_hpmv]].
+!> See also: [[mfi_hpmv]], [[f77_hpmv]].
+pure subroutine mfi_zhpmv(ap, x, y, uplo, alpha, beta, incx, incy)
+    integer, parameter :: wp = REAL64
+    complex(REAL64), intent(in) :: x(:)
+    complex(REAL64), intent(in) :: ap(:)
+    complex(REAL64), intent(inout) :: y(:)
+    character, intent(in), optional :: uplo
+    character :: local_uplo
+    complex(REAL64), intent(in), optional :: alpha
+    complex(REAL64) :: local_alpha
+    complex(REAL64), intent(in), optional :: beta
+    complex(REAL64) :: local_beta
+    integer, intent(in), optional :: incx
+    integer :: local_incx
+    integer, intent(in), optional :: incy
+    integer :: local_incy
+    integer :: n
+    if (present(uplo)) then
+        local_uplo = uplo
+    else
+        local_uplo = 'U'
+    end if
+    if (present(alpha)) then
+        local_alpha = alpha
+    else
+        local_alpha = 1.0_wp
+    end if
+    if (present(beta)) then
+        local_beta = beta
+    else
+        local_beta = 0.0_wp
+    end if
+    if (present(incx)) then
+        local_incx = incx
+    else
+        local_incx = 1
+    end if
+    if (present(incy)) then
+        local_incy = incy
+    else
+        local_incy = 1
+    end if
+    n = size(x)
+    call f77_hpmv(local_uplo,n,local_alpha,ap,x,local_incx,local_beta,y,local_incy)
+end subroutine
 end module
 
--- src/tbsv.f90
+++ src/tbsv.f90
@@ -1,72 +1,187 @@
-module f77_blas_tbsv
+module mfi_blas_tbsv
     use iso_fortran_env
+    use f77_blas
+#if defined(MFI_CUBLAS)
     use iso_c_binding
+    use mfi_blas_cublas
+#endif
+#if defined(MFI_EXTENSIONS)
+    use mfi_blas_extensions
+#endif
     implicit none
 
-!> Generic old style interface for TBSV.
+!> Generic modern interface for TBSV.
 !> Supports s, d, c, z.
-!> See also: [[mfi_tbsv]], [[stbsv]], [[dtbsv]], [[ctbsv]], [[ztbsv]].
-interface f77_tbsv
-!> Original interface for STBSV
-!> See also: [[mfi_tbsv]], [[tbsv]].
-pure subroutine stbsv(uplo, trans, diag, n, k, a, lda, x, incx)
-    import :: REAL32
-    integer, parameter :: wp = REAL32
-    real(REAL32), intent(in) :: a(lda,*)
-    real(REAL32), intent(inout) :: x(*)
-    character, intent(in) :: uplo
-    character, intent(in) :: trans
-    character, intent(in) :: diag
-    integer, intent(in) :: n
-    integer, intent(in) :: k
-    integer, intent(in) :: lda
-    integer, intent(in) :: incx
-end subroutine
-!> Original interface for DTBSV
-!> See also: [[mfi_tbsv]], [[tbsv]].
-pure subroutine dtbsv(uplo, trans, diag, n, k, a, lda, x, incx)
-    import :: REAL64
-    integer, parameter :: wp = REAL64
-    real(REAL64), intent(in) :: a(lda,*)
-    real(REAL64), intent(inout) :: x(*)
-    character, intent(in) :: uplo
-    character, intent(in) :: trans
-    character, intent(in) :: diag
-    integer, intent(in) :: n
-    integer, intent(in) :: k
-    integer, intent(in) :: lda
-    integer, intent(in) :: incx
-end subroutine
-!> Original interface for CTBSV
-!> See also: [[mfi_tbsv]], [[tbsv]].
-pure subroutine ctbsv(uplo, trans, diag, n, k, a, lda, x, incx)
-    import :: REAL32
-    integer, parameter :: wp = REAL32
-    complex(REAL32), intent(in) :: a(lda,*)
-    complex(REAL32), intent(inout) :: x(*)
-    character, intent(in) :: uplo
-    character, intent(in) :: trans
-    character, intent(in) :: diag
-    integer, intent(in) :: n
-    integer, intent(in) :: k
-    integer, intent(in) :: lda
-    integer, intent(in) :: incx
-end subroutine
-!> Original interface for ZTBSV
-!> See also: [[mfi_tbsv]], [[tbsv]].
-pure subroutine ztbsv(uplo, trans, diag, n, k, a, lda, x, incx)
-    import :: REAL64
-    integer, parameter :: wp = REAL64
-    complex(REAL64), intent(in) :: a(lda,*)
-    complex(REAL64), intent(inout) :: x(*)
-    character, intent(in) :: uplo
-    character, intent(in) :: trans
-    character, intent(in) :: diag
-    integer, intent(in) :: n
-    integer, intent(in) :: k
-    integer, intent(in) :: lda
-    integer, intent(in) :: incx
-end subroutine
+!> See also:
+!> [[f77_tbsv:stbsv]], [[f77_tbsv:dtbsv]], [[f77_tbsv:ctbsv]], [[f77_tbsv:ztbsv]].
+interface mfi_tbsv
+    module procedure :: mfi_stbsv
+    module procedure :: mfi_dtbsv
+    module procedure :: mfi_ctbsv
+    module procedure :: mfi_ztbsv
 end interface
+
+contains
+
+!> Modern interface for [[f77_tbsv:f77_tbsv]].
+!> See also: [[mfi_tbsv]], [[f77_tbsv]].
+pure subroutine mfi_stbsv(a, x, uplo, trans, diag, incx)
+    integer, parameter :: wp = REAL32
+    real(REAL32), intent(in) :: a(:,:)
+    real(REAL32), intent(inout) :: x(:)
+    character, intent(in), optional :: uplo
+    character :: local_uplo
+    character, intent(in), optional :: trans
+    character :: local_trans
+    character, intent(in), optional :: diag
+    character :: local_diag
+    integer, intent(in), optional :: incx
+    integer :: local_incx
+    integer :: n, k, lda
+    if (present(uplo)) then
+        local_uplo = uplo
+    else
+        local_uplo = 'U'
+    end if
+    if (present(trans)) then
+        local_trans = trans
+    else
+        local_trans = 'N'
+    end if
+    if (present(diag)) then
+        local_diag = diag
+    else
+        local_diag = 'N'
+    end if
+    if (present(incx)) then
+        local_incx = incx
+    else
+        local_incx = 1
+    end if
+    k = size(a,1)-1
+    lda = max(1,size(a,1))
+    n = size(a,2)
+    call f77_tbsv(local_uplo,local_trans,local_diag,n,k,a,lda,x,local_incx)
+end subroutine
+!> Modern interface for [[f77_tbsv:f77_tbsv]].
+!> See also: [[mfi_tbsv]], [[f77_tbsv]].
+pure subroutine mfi_dtbsv(a, x, uplo, trans, diag, incx)
+    integer, parameter :: wp = REAL64
+    real(REAL64), intent(in) :: a(:,:)
+    real(REAL64), intent(inout) :: x(:)
+    character, intent(in), optional :: uplo
+    character :: local_uplo
+    character, intent(in), optional :: trans
+    character :: local_trans
+    character, intent(in), optional :: diag
+    character :: local_diag
+    integer, intent(in), optional :: incx
+    integer :: local_incx
+    integer :: n, k, lda
+    if (present(uplo)) then
+        local_uplo = uplo
+    else
+        local_uplo = 'U'
+    end if
+    if (present(trans)) then
+        local_trans = trans
+    else
+        local_trans = 'N'
+    end if
+    if (present(diag)) then
+        local_diag = diag
+    else
+        local_diag = 'N'
+    end if
+    if (present(incx)) then
+        local_incx = incx
+    else
+        local_incx = 1
+    end if
+    k = size(a,1)-1
+    lda = max(1,size(a,1))
+    n = size(a,2)
+    call f77_tbsv(local_uplo,local_trans,local_diag,n,k,a,lda,x,local_incx)
+end subroutine
+!> Modern interface for [[f77_tbsv:f77_tbsv]].
+!> See also: [[mfi_tbsv]], [[f77_tbsv]].
+pure subroutine mfi_ctbsv(a, x, uplo, trans, diag, incx)
+    integer, parameter :: wp = REAL32
+    complex(REAL32), intent(in) :: a(:,:)
+    complex(REAL32), intent(inout) :: x(:)
+    character, intent(in), optional :: uplo
+    character :: local_uplo
+    character, intent(in), optional :: trans
+    character :: local_trans
+    character, intent(in), optional :: diag
+    character :: local_diag
+    integer, intent(in), optional :: incx
+    integer :: local_incx
+    integer :: n, k, lda
+    if (present(uplo)) then
+        local_uplo = uplo
+    else
+        local_uplo = 'U'
+    end if
+    if (present(trans)) then
+        local_trans = trans
+    else
+        local_trans = 'N'
+    end if
+    if (present(diag)) then
+        local_diag = diag
+    else
+        local_diag = 'N'
+    end if
+    if (present(incx)) then
+        local_incx = incx
+    else
+        local_incx = 1
+    end if
+    k = size(a,1)-1
+    lda = max(1,size(a,1))
+    n = size(a,2)
+    call f77_tbsv(local_uplo,local_trans,local_diag,n,k,a,lda,x,local_incx)
+end subroutine
+!> Modern interface for [[f77_tbsv:f77_tbsv]].
+!> See also: [[mfi_tbsv]], [[f77_tbsv]].
+pure subroutine mfi_ztbsv(a, x, uplo, trans, diag, incx)
+    integer, parameter :: wp = REAL64
+    complex(REAL64), intent(in) :: a(:,:)
+    complex(REAL64), intent(inout) :: x(:)
+    character, intent(in), optional :: uplo
+    character :: local_uplo
+    character, intent(in), optional :: trans
+    character :: local_trans
+    character, intent(in), optional :: diag
+    character :: local_diag
+    integer, intent(in), optional :: incx
+    integer :: local_incx
+    integer :: n, k, lda
+    if (present(uplo)) then
+        local_uplo = uplo
+    else
+        local_uplo = 'U'
+    end if
+    if (present(trans)) then
+        local_trans = trans
+    else
+        local_trans = 'N'
+    end if
+    if (present(diag)) then
+        local_diag = diag
+    else
+        local_diag = 'N'
+    end if
+    if (present(incx)) then
+        local_incx = incx
+    else
+        local_incx = 1
+    end if
+    k = size(a,1)-1
+    lda = max(1,size(a,1))
+    n = size(a,2)
+    call f77_tbsv(local_uplo,local_trans,local_diag,n,k,a,lda,x,local_incx)
+end subroutine
 end module
 
