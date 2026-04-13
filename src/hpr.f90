6b0f33aca6bc36b858c28ad640b5cf2e4cbbba4d
14NGiestas <iangiestaspauli@gmail.com>
2026-04-13 08:47:37 +0000

    Deploying to docs from @ 14NGiestas/mfi@f50b3deba082f170646e8ec90741197e280e099d 🚀

--- src/hpr.f90
+++ src/hpr.f90
@@ -1,87 +1,36 @@
-module mfi_blas_hpr
+module f77_blas_hpr
     use iso_fortran_env
-    use f77_blas
-#if defined(MFI_CUBLAS)
     use iso_c_binding
-    use mfi_blas_cublas
-#endif
-#if defined(MFI_EXTENSIONS)
-    use mfi_blas_extensions
-#endif
     implicit none
 
-!> Generic modern interface for HPR.
+!> Generic old style interface for HPR.
 !> Supports c, z.
-!> See also:
-!> [[f77_hpr:chpr]], [[f77_hpr:zhpr]].
-interface mfi_hpr
-    module procedure :: mfi_chpr
-    module procedure :: mfi_zhpr
-end interface
-
-contains
-
-!> Modern interface for [[f77_hpr:f77_hpr]].
-!> See also: [[mfi_hpr]], [[f77_hpr]].
-pure subroutine mfi_chpr(ap, x, uplo, alpha, incx)
+!> See also: [[mfi_hpr]], [[chpr]], [[zhpr]].
+interface f77_hpr
+!> Original interface for CHPR
+!> See also: [[mfi_hpr]], [[hpr]].
+pure subroutine chpr(uplo, n, alpha, x, incx, ap)
+    import :: REAL32
     integer, parameter :: wp = REAL32
-    complex(REAL32), intent(in) :: x(:)
-    complex(REAL32), intent(inout) :: ap(:)
-    character, intent(in), optional :: uplo
-    character :: local_uplo
-    real(wp), intent(in), optional :: alpha
-    real(wp) :: local_alpha
-    integer, intent(in), optional :: incx
-    integer :: local_incx
-    integer :: n
-    if (present(uplo)) then
-        local_uplo = uplo
-    else
-        local_uplo = 'U'
-    end if
-    if (present(alpha)) then
-        local_alpha = alpha
-    else
-        local_alpha = 1.0_wp
-    end if
-    if (present(incx)) then
-        local_incx = incx
-    else
-        local_incx = 1
-    end if
-    n = size(x)
-    call f77_hpr(local_uplo,n,local_alpha,x,local_incx,ap)
+    complex(REAL32), intent(in) :: x(*)
+    complex(REAL32), intent(inout) :: ap(*)
+    character, intent(in) :: uplo
+    real(wp), intent(in) :: alpha
+    integer, intent(in) :: n
+    integer, intent(in) :: incx
 end subroutine
-!> Modern interface for [[f77_hpr:f77_hpr]].
-!> See also: [[mfi_hpr]], [[f77_hpr]].
-pure subroutine mfi_zhpr(ap, x, uplo, alpha, incx)
+!> Original interface for ZHPR
+!> See also: [[mfi_hpr]], [[hpr]].
+pure subroutine zhpr(uplo, n, alpha, x, incx, ap)
+    import :: REAL64
     integer, parameter :: wp = REAL64
-    complex(REAL64), intent(in) :: x(:)
-    complex(REAL64), intent(inout) :: ap(:)
-    character, intent(in), optional :: uplo
-    character :: local_uplo
-    real(wp), intent(in), optional :: alpha
-    real(wp) :: local_alpha
-    integer, intent(in), optional :: incx
-    integer :: local_incx
-    integer :: n
-    if (present(uplo)) then
-        local_uplo = uplo
-    else
-        local_uplo = 'U'
-    end if
-    if (present(alpha)) then
-        local_alpha = alpha
-    else
-        local_alpha = 1.0_wp
-    end if
-    if (present(incx)) then
-        local_incx = incx
-    else
-        local_incx = 1
-    end if
-    n = size(x)
-    call f77_hpr(local_uplo,n,local_alpha,x,local_incx,ap)
+    complex(REAL64), intent(in) :: x(*)
+    complex(REAL64), intent(inout) :: ap(*)
+    character, intent(in) :: uplo
+    real(wp), intent(in) :: alpha
+    integer, intent(in) :: n
+    integer, intent(in) :: incx
 end subroutine
+end interface
 end module
 
